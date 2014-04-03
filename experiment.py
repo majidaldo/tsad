4deba5191f303031ec1ec898c79df28daf7666a3
majidaldo <majidaldosar-github@yahoo.com>
2014-04-02 21:31:24 -0400

    can reconstruct even but not odd
    ..number of waves

--- experiment.py
+++ experiment.py
@@ -3,22 +3,38 @@ import scipy as sp
 from sklearn import decomposition as decomp
 
 xs=np.linspace(0,1,10e3)
-ts=np.sin(xs*100*2*np.pi)+np.cos(xs*50*2*np.pi)
 
-nts=np.reshape(ts,(10,int(1e3)))
-ntsf=np.empty((10,len(ts)/10))#noisy ts
-for atsi in xrange(len(nts)):
-    cts=ts[atsi*1000:(atsi+1)*1000]
-    ntsf[atsi]=cts#\
-    #+.05*sp.std(cts)*sp.random.randn(len(cts))
-    
+def sinewave(xs,**kwargs):
+    nw=kwargs.setdefault('nw',100) #num waves
+    return np.sin(xs*nw*2*np.pi)
 
-#if the same sample then should reproduce
+def const(xs,**kwargs):
+    c=kwargs.setdefault('c',0)
+    cs=np.empty(len(xs));cs.fill(c)
+    return cs
+
+nts=np.empty((10,len(xs)))
+for i in xrange(len(nts)):
+    nts[i]=sinewave(xs,nw=(i+1)*2)
     
-pd=decomp.PCA(n_components=5)
+nc=10#len(nts)   
+pd=decomp.PCA(n_components=nc)
 #pd=decomp.RandomizedPCA(n_components=3)#'mle')
-#pd=decomp.FastICA(n_components=3)
-
+#pd=decomp.FastICA(max_iter=int(1e3),n_components=nc)
+#pd=decomp.SparsePCA(n_components=nc,n_jobs=4) #NOT sparse coding! just alot of zeros
 pdc=pd.fit(nts)
+sigspc=pdc.components_
 
+#find which u/s scheme gives least number of components
+x=np.array([sinewave(xs,nw=20)])
+#x=np.array([const(xs,c=3)]) #zero is trivial though
 
+sc=decomp.sparse_encode( x,  sigspc )
+
+#scc=sc.transform( x  )
+#sc=decomp.SparseCoder(np.array(  [[1.,0],[333,0]]   ))
+#scc=sc.transform( np.array( [[3,4]]  )   )
+
+#need to be careful about orthogonal. it the folowing ok?
+#reconstruct signal
+recon=np.dot(sc,sigspc)
