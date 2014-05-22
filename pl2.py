d6ab296573e85e58e7a1f2d9fd3ef15ac8fc0af5
majidaldo <majidaldosar-github@yahoo.com>
2014-05-22 16:17:17 -0400

    can reconstruct training sine waves

--- pl2.py
+++ pl2.py
@@ -13,10 +13,10 @@ def const(xs,**kwargs):
     return cs
 
 #generate an even number of waves in the re
-nts=np.empty((100,len(xs)),dtype='float32')
+nts=np.empty((70,len(xs)),dtype='float32')
 for i in xrange(len(nts)):
-    nts[i]=sinewave(xs,nw=(i+1)*2)
-nts[-1]=const(xs,c=1000)
+    nts[i]=sinewave(xs,nw=(i+1)*2)/2+.5 #bc the algo just in rng [0,1]
+#nts[-1]=const(xs,c=1)#=1000) #this was messing things up!
 
 import pylearn2
 from pylearn2 import datasets
@@ -26,23 +26,44 @@ from pylearn2.training_algorithms import sgd
 from pylearn2.costs import autoencoder
 from pylearn2 import termination_criteria
 
-ds=pylearn2.datasets.dense_design_matrix.DenseDesignMatrix(
-    X=nts
+
+trn_prop=.7
+np.random.shuffle(nts)
+ntrn=((len(nts))*float(trn_prop))
+nval=len(nts)-ntrn
+dst=pylearn2.datasets.dense_design_matrix.DenseDesignMatrix(
+    X=nts[:ntrn]
     )
+dsv=pylearn2.datasets.dense_design_matrix.DenseDesignMatrix(
+    X=nts[:nval]
+    )
+ds= pylearn2.datasets.dense_design_matrix.DenseDesignMatrix(
+    X=nts
+)
 mdl=pylearn2.models.autoencoder.Autoencoder(
-    ds.X_space.dim
-    ,ds.X_space.dim
+    dst.X_space.dim #input dim
+    ,50#ds.X_space.dim #n hidden 
     ,'sigmoid'
     ,'sigmoid'
+    ,tied_weights=True
     )
 algo=pylearn2.training_algorithms.sgd.SGD(
     .05
     ,cost=pylearn2.costs.autoencoder.MeanSquaredReconstructionError()#.cost locks up puter!
-    ,termination_criterion=termination_criteria.EpochCounter(10000)
+    #,termination_criterion=termination_criteria.MonitorBased(
+    #    channel_name='objective',prop_decrease=.01,N=10)#chnl def in SGD
+    ,termination_criterion=termination_criteria.EpochCounter(1000)
+    ,monitoring_dataset=ds
     ,batch_size=1#10?100?
     )
 
-trn=pylearn2.train.Train(ds,mdl,algorithm=algo)
-#do i need a Y for autoencoder?
+from os.path import curdir
+from os.path import abspath
+trn=pylearn2.train.Train(ds
+                         ,mdl
+                         ,algorithm=algo
+                         ,save_path=abspath(curdir))
 
-#to train: trn.main_loop()
+from theano import tensor
+def train(): return trn.main_loop()
+def recon(): return trn.model.reconstruct(tensor.constant(nts[:])).eval()
