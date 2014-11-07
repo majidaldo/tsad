b0ae26d881aa1ef35a01efae222f9aa849a6f908
majidaldo <majidaldosari-github@yahoo.com>
2014-11-07 16:11:49 -0500

    beginning of test rnn

--- testrnn.py
+++ testrnn.py
@@ -6,10 +6,12 @@ from window import winbatch
 
 e = theanets.Experiment(
     theanets.recurrent.Regressor,
-    layers=(1, 10, 1),
-    train_batches=16
-    ,batch_size=16 #whats the difference?
+    layers=(1, 10, 1)
+    ,train_batches=16
+    #,batch_size=16 #whats the difference?
     )
 
-ecgb=winbatch(np.loadtxt('ecg.txt',dtype='f32'),batch_size=e.args.batch_size)
-#e.run(sum_waves, sum_waves)
+ecgb=winbatch(np.loadtxt('ecg.txt',dtype='f32')[:,None]
+              ,batch_size=e.args.batch_size)
+#e.run(ecgb, ecgb)
+#predict(batch)
