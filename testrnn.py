625787dcd5a4b17693b7fbd2399e0bf2003ee03e
majidaldo <majidaldosari-github@yahoo.com>
2014-11-07 14:57:12 -0500

    first attempt to run my rnn for a real timeseries

---
+++ testrnn.py
@@ -0,0 +1,15 @@
+import theanets
+import numpy as np
+
+from window import winbatch
+
+
+e = theanets.Experiment(
+    theanets.recurrent.Regressor,
+    layers=(1, 10, 1),
+    train_batches=16
+    ,batch_size=16 #whats the difference?
+    )
+
+ecgb=winbatch(np.loadtxt('ecg.txt',dtype='f32'),batch_size=e.args.batch_size)
+#e.run(sum_waves, sum_waves)
