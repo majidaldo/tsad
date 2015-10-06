e4198875af6579701a1fbf6dba86ce01f69fa74f
Majid d <majidaldosari-github@yahoo.com>
2015-10-05 21:48:24 -0400

    chdir

--- experiments/sleep/o.py
+++ experiments/sleep/o.py
@@ -1,13 +1,11 @@
-import sys
-sys.path.append('../..')
-from omain import *
-
-
 import json
 with open('config.json') as cf:
     xpnm=json.load(cf)['experiment-name']
 
+import sys
+sys.path.append('../..')
+from omain import *
 
-env(xpnm) 
-
-
+import os
+os.chdir(os.path.join('..','..'))
+env(xpnm)
