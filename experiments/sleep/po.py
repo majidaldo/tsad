91fcbfa4004a8e8de0c62d33e9ec2d4e7968c2dd
Majid d <majidaldosari-github@yahoo.com>
2015-10-09 11:22:21 -0400

    printing stdout from engines

--- experiments/sleep/po.py
+++ experiments/sleep/po.py
@@ -1,6 +1,8 @@
 import json
 with open('config.json') as cf:
-    xpnm=json.load(cf)['experiment-name']
+    jl=json.load(cf)
+    xpnm=jl['experiment-name']
+    db=jl['database']['address']
 
 import sys
 import os
@@ -11,6 +13,13 @@ def main(job_id,params):
     c=pomain.connect()
     r=pomain.lbv.apply_sync(pomain.main,xpnm
                           ,job_id,params)
-    #load balanced view: just one engine gets it
-    #r=pomain.lbv.apply_sync(lambda x:  float(x),1)
+    #lbv.get_result(-1) #not working!
+    #so getting into the db itself
+    import pymongo#3
+    mc=pymongo.MongoClient(db)
+    ipt=mc['ipython-tasks']['task_records']
+    #unicode baby !
+    rec=ipt.find_one({u'msg_id':pomain.lbv.history[-1]})
+    print rec['stdout']
+    print rec['stderr']
     return r
